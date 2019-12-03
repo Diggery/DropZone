343f78d3ce0e7f8567dd86312c1e7ac36c2be6ce
Brian Collins <collins.brian@gmail.com>
2019-12-02 20:28:01 -0800

    added map selector

---
+++ Assets/DropZone/Code/MapSelector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSelector : MonoBehaviour {
+
+  bool isOpen = false;
+  Animator animator;
+  Material selectorMat;
+
+
+  public bool IsOpen {
+    get { return IsOpen; }
+    set { isOpen = value; }
+  }
+
+
+  void Start() {
+
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+}
